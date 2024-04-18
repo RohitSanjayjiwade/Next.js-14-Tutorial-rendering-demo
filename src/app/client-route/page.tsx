7f0a947d48268e9a476cbab95fa6ec2eec3a9298
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-04-18 13:40:55 +0530

    Server-only Code

---
+++ src/app/client-route/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import {serverSideFunction} from "@/utils/server-utils"
+
+export default function ClientRoutePage(){
+	const result = serverSideFunction();
+	return (
+		<>
+			<h1>ClientRoutePage</h1>
+			<p>{result}</p>
+		</>
+		);
+}
