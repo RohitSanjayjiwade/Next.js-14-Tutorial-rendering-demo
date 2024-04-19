60a2ea777b0dc710b258b5c778e54d6396619ccf
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-04-19 19:16:01 +0530

    Client-only Code

--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,4 +1,5 @@
 import {serverSideFunction} from "@/utils/server-utils"
+import {clientSideFunction} from "@/utils/client-utils"
 
 import {ImageSlider} from "@/components/ImageSlider";
 
@@ -7,10 +8,12 @@ import {ImageSlider} from "@/components/ImageSlider";
 
 export default function ServerRoutePage(){
 	const result = serverSideFunction()
+	const result2 = clientSideFunction()
 	return (
 		<>
 			<h1>ServerRoutePage</h1>
 			<p>{result}</p>
+			<p>{result2}</p>
 			<ImageSlider />
 		</>
 		);
